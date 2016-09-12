d094a0bbe8a98062c925026285289851710beff9
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-12 14:48:53 +0100

    Exercise 8 - complete step 4

--- student_directory/directory.rb
+++ student_directory/directory.rb
@@ -4,28 +4,54 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
+  puts "Add country of birth:"
+  country = gets.chomp.capitalize
+  puts "Which cohort are you joining?"
+  cohort = gets.chomp.capitalize
+  puts "Add gender:"
+  gender = gets.chomp
+  puts "Hobbies - seperate with a comma:"
+  hobbies = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, country: country, gender: gender, hobbies: hobbies}
     puts "Now we have #{students.count} students"
     # gets another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
+    puts "Country of birth:"
+    country = gets.chomp.capitalize
+    puts "Cohort:"
+    cohort = gets.chomp
+    puts "Gender:"
+    gender = gets.chomp
+    puts "Hobbies:"
+    hobbies = gets.chomp
   end
   # return the array of students
   students
 end
 
  def print_header
-   puts "The students of Villans Academy"
-   puts " -------------"
+   puts "The students of Villans Academy".center(50)
+   puts " -------------".center(50)
  end
 
  # def print(students)
  #   students.each do |student|
- #     if student ==
- #     puts "#{student[:name]} (#{student[:cohort]} cohort)"
+ #     puts "#{student[:name]} (#{student[:cohort]} cohort): Country of birth: #{student[:country]}. Gender: #{student[:gender]}. Hobbies: #{student[:hobbies]}".center(150)
+ #   end
+ # end
+
+ # Exercies 8 - No 4:
+ # def print(students)
+ #   count = 0
+ #   until count == students.length
+ #   students.each do |student|
+ #       puts "#{student[:name]} (#{student[:cohort]} cohort)"
+ #       count += 1
+ #     end
  #   end
  # end
 
@@ -38,13 +64,15 @@ end
 #   end
 # end
 
-def print(students)
-  students.each do |student|
-    if student[:name[0]] == "D"
-      puts "#{student[:name]}"
-    end
-  end
-end
+
+# Exercise 8 - No 2:
+# def print(students)
+#   students.each do |student|
+#     if student[:name].start_with? "D"
+#       puts "#{student[:name]}"
+#     end
+#   end
+# end
 
 # Exercise 8 - No 1:
 # def print(students)
@@ -53,8 +81,9 @@ end
 #    end
 # end
 
+
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 #nothing happens until we call the methods
 students = input_students
