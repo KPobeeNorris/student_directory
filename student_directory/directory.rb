d8c5cc07e84451ff4f56127fc418dc1c2c1bec15
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-13 14:51:33 +0100

    Week 3 - step 8 - rework of question section

--- student_directory/directory.rb
+++ student_directory/directory.rb
@@ -3,54 +3,67 @@ def input_students
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
+  while true
   # get the first name
   name = gets.chomp.capitalize
+  if name.empty?
+    break
+  else
   puts "Add country of birth:"
   country = gets.chomp.capitalize
   puts "Which cohort are you joining?"
   cohort = gets.chomp.capitalize
-  puts "Add gender:"
-  gender = gets.chomp
-  puts "Hobbies - seperate with a comma:"
-  hobbies = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: cohort, country: country, gender: gender, hobbies: hobbies}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
-    elsif students.count > 1
-      puts "Now we have #{students.count} students"
-    end
-    # gets another name from the user
-    name = gets.chomp.capitalize
-      if name != ''
-        puts "Country of birth:"
-        country = gets.chomp.capitalize
-        puts "Cohort:"
-        cohort = gets.chomp
-        puts "Gender:"
-        gender = gets.chomp
-        puts "Hobbies:"
-        hobbies = gets.chomp
-      end
-
-
+  #add the student hash to the array
+  students << {
+    name: name,
+    cohort: cohort,
+    country: country
+  }
   end
+  if students.count == 1
+    puts "Now we have #{students.count} student"
+  elsif students.count > 1
+    puts "Now we have #{students.count} students"
+  end
+end
+
   # return the array of students
   students
 end
 
  def print_header
-   puts "The students of Villans Academy".center(100)
+   puts "The students of Villains Academy".center(100)
    puts " -------------".center(100)
  end
 
  def print(students)
    students.each do |student|
-     puts "#{student[:name]} (#{student[:cohort]} cohort): Country of birth: #{student[:country]}. Gender: #{student[:gender]}. Hobbies: #{student[:hobbies]}".center(100)
+     puts "#{student[:name]} (#{student[:cohort]} cohort): Country of birth: #{student[:country]}".center(100)
    end
  end
+
+def print_footer(names)
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student".center(100)
+  elsif names.count > 1
+    puts "Overall, we have #{names.count} great students".center(100)
+  end
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+#cosort(students)
+print_footer(students)
+
+
+
+
+
+# Exercise 8 - No 8:
+#  def cosort(students)
+#    puts students.group_by {|a| a[:cohort]}.sort
+#  end
 
  # Exercies 8 - No 4:
  # Description: alternative way of printing our all students
@@ -69,7 +82,7 @@ end
 # def print(students)
 #   students.each do |student|
 #     if student[:name].length < 12
-#       puts "#{student[:name]} (#{student[:cohort]} cohort)"
+#       puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
 #     end
 #   end
 # end
@@ -80,7 +93,7 @@ end
 # def print(students)
 #   students.each do |student|
 #     if student[:name].start_with? "D"
-#       puts "#{student[:name]}"
+#       puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
 #     end
 #   end
 # end
@@ -88,20 +101,6 @@ end
 # Exercise 8 - No 1:
 # def print(students)
 #    students.each_with_index do |student, index|
-#      puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+#      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(100)
 #    end
 # end
-
-
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student".center(100)
-  elsif names.count > 1
-    puts "Overall, we have #{names.count} great students".center(100)
-  end
-end
-#nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
