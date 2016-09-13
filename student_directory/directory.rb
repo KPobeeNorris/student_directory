f463ac0bcdf647c91a51a651902e2eae57c55810
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-13 19:17:45 +0100

    Step 9 - interactive menu completed

--- student_directory/directory.rb
+++ student_directory/directory.rb
@@ -1,5 +1,5 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name(s) of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
@@ -20,7 +20,8 @@ def input_students
     cohort = 'TBC'
   end
   #add the student hash to the array
-  puts "You've entered the following: Name - #{name}, Country -#{country} and ohort - #{cohort}.  Do you want to commit this data (Y/N)?"
+  puts "You've entered the following: Name - #{name}, Country -#{country} and Cohort - #{cohort}." +
+   " Is this data correct? (Y/N)?"
   answer = gets.chomp.upcase
   if answer == 'N'
     break
@@ -44,7 +45,7 @@ end
 end
 
  def print_header
-   puts "The students of Villains Academy".center(100)
+   puts "The Students of Villains Academy".center(100)
    puts " -------------".center(100)
  end
 
@@ -61,7 +62,33 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students".center(100)
   end
 end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it to a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else "I don't know what you meant, try again"
+    end
+  end
+end
+
 #nothing happens until we call the methods
+interactive_menu
 students = input_students
 if !students.empty?
   print_header
